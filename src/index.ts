894b8bc0074c601dfedf1bbdcee3b3fdc661a523
Zoey <zoey@panties.moe>
2022-01-19 11:31:53 -0500

    Import native module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as Commands from "./modules/commands";
 import * as Clyde from "./modules/clyde";
 import * as Users from "./modules/users";
+import * as Native from "./modules/native";
 
 export {
   Commands,
   Clyde,
   Users,
+  Native
 }
