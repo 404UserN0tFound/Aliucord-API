02595f3720a7bf3c79a8619c2ddd0a524b4ce988
Zoey <zoey@panties.moe>
2022-01-19 13:11:48 -0500

    Import dialog module

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as Commands from "./modules/commands";
 import * as Clyde from "./modules/clyde";
 import * as Users from "./modules/users";
 import * as Native from "./modules/native";
+import * as Dialog from "./modules/dialog";
 
 export {
   Commands,
   Clyde,
   Users,
-  Native
+  Native,
+  Dialog
 }
