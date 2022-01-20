dc28965017c694b2e72cabddd6017285e105e0e5
Zoey <zoey@panties.moe>
2022-01-19 23:02:12 -0500

    Add helper function for native

--- src/modules/native.ts
+++ src/modules/native.ts
@@ -7,6 +7,39 @@ function reloadDiscord() {
   nativeModules.BundleUpdaterManager.reload();
 }
 
+/**
+ * Get the Discord's version
+ */
+function getVersion() {
+  return nativeModules.InfoDictionaryManager.Version;
+}
+
+/**
+ * Get the Discord's build number
+ */
+function getBuild() {
+  return nativeModules.InfoDictionaryManager.Build;
+}
+
+/**
+ * Get the user's device
+ */
+function getDevice() {
+  return nativeModules.DCDDeviceManager.device;
+}
+
+/**
+ * Get the user's device version
+ */
+function getSystemVersion() {
+  return nativeModules.DCDDeviceManager.systemVersion;
+}
+
 export {
-  reloadDiscord
+  reloadDiscord,
+
+  getVersion,
+  getBuild,
+  getDevice,
+  getSystemVersion
 }
