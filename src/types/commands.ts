610007c3d748db9db3d9411fce3fbf51c8abf374
Zoey <zoey@panties.moe>
2022-01-23 20:43:11 -0500

    Support for newer builds of Discord

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -87,8 +87,8 @@ export {
   Argument,
 
   ApplicationCommandSectionType,
-  ApplicationCommandTarget,
   ApplicationCommandType,
+  ApplicationCommandInputType,
   ApplicationCommandPermissionType,
   ApplicationCommandOptionType,
 
