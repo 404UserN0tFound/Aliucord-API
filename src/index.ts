2c535ca8f0b94d550ac9f0c8a44b333244f0984e
Zoey <zoey@panties.moe>
2022-02-10 14:41:54 -0500

    Add index

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import * as Clyde from "./modules/clyde";
+import * as Commands from "./modules/commands";
+import * as Dialog from "./modules/dialog";
+import * as Module from "./modules/module";
+import * as Native from "./modules/native";
+import * as Patcher from "./modules/patcher";
+import * as Rest from "./modules/rest";
+import * as Storage from "./modules/storage";
+import * as Themes from "./modules/themes";
+import * as Toast from "./modules/toast";
+import * as Token from "./modules/token";
+import * as Users from "./modules/users";
+
+export {
+  Clyde,
+  Commands,
+  Dialog,
+  Module,
+  Native,
+  Patcher,
+  Rest,
+  Storage,
+  Themes,
+  Toast,
+  Token,
+  Users,
+}
